741d043e31fca7b554d09772af1a1c9f5825b00b
Dave Poirier <dave@encoded.life>
2025-11-08 21:40:18 -0500

    Initial Commit

---
+++ SevenWondersDice/SevenWondersDiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  SevenWondersDiceApp.swift
+//  SevenWondersDice
+//
+//  Created by Dave Poirier on 2025-11-08.
+//
+
+import SwiftUI
+
+@main
+struct SevenWondersDiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
